8f23266ba5615e717e889b1d9331d9d851afa42b
AyoubBazaz <ayoubbazaz02@gmail.com>
2025-01-29 16:31:04 +0100

    Done

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const port = 3000;
 const db = mysql.createConnection({
   host: 'localhost', 
   user: 'root', 
-  password: 'ayoubBAZAZ@123456', 
+  password: 'password', 
   database: 'clickFit'
 });
 
